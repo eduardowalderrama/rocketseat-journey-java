2f30ce08f8057a44ab377e9c6a84a2f3ea1a92ec
eduardowalderrama <walderramaeduardo@gmail.com>
2024-10-12 15:00:05 -0300

    operadores relacionais

---
+++ first-project/src/operadores/Relacionais.java
@@ -0,0 +1,23 @@
+package operadores;
+
+public class Relacionais {
+    public static void main(String[] args) {
+        int numero1 = 1;
+        int numero2 = 2;
+
+        if(numero1 > numero2)
+            System.out.print("Numero 1 maior que numero 2");
+
+        if(numero1 < numero2)
+            System.out.print("Numero 1 menor que numero 2");
+
+        if(numero1 >= numero2)
+            System.out.print("Numero 1 maior ou igual que numero 2");
+
+        if(numero1 <= numero2)
+            System.out.print("Numero 1 menor ou igual que numero 2");
+
+        if(numero1 != numero2)
+            System.out.print("Numero 1 eh diferente de numero 2");
+    }
+}
